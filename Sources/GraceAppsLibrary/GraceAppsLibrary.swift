3411dbef87aa58f759579d2e4c71dace940e2ec9
higracehuang <higracehuang@gmail.com>
2026-01-20 11:48:55 -0600

    Refactor

--- Sources/GraceAppsLibrary/GraceAppsLibrary.swift
+++ Sources/GraceAppsLibrary/GraceAppsLibrary.swift
@@ -69,108 +69,108 @@ public struct GraceApp: Hashable {
 }
 
 public enum GraceAppsLibrary {
-    public static func getAllApps(excluding appIdToExclude: String? = nil) -> [GraceApp] {
-        let allApps = [
-            GraceApp(
-                name: "app.name.tallycoin",
-                iconName: "TallyCoinIcon",
-                shortDescription: "app.description.tallycoin",
-                appId: "id1633932632",
-                releaseDate: Calendar.current.date(from: DateComponents(year: 2022, month: 7, day: 11))!,
-                category: .productivity
-            ),
-            GraceApp(
-                name: "app.name.readingclock",
-                iconName: "ReadingClockIcon",
-                shortDescription: "app.description.readingclock",
-                appId: "id6473550400",
-                releaseDate: Calendar.current.date(from: DateComponents(year: 2023, month: 11, day: 30))!,
-                category: .productivity
-            ),
-            GraceApp(
-                name: "app.name.stemcards",
-                iconName: "StemCardsIcon",
-                shortDescription: "app.description.stemcards",
-                appId: "id6478243260",
-                releaseDate: Calendar.current.date(from: DateComponents(year: 2024, month: 2, day: 27))!,
-                category: .education
-            ),
-            GraceApp(
-                name: "app.name.stitchtally",
-                iconName: "StitchTallyIcon",
-                shortDescription: "app.description.stitchtally",
-                appId: "id6738016114",
-                releaseDate: Calendar.current.date(from: DateComponents(year: 2024, month: 11, day: 12))!,
-                category: .lifestyle
-            ),
-            GraceApp(
-                name: "app.name.quizmeai",
-                iconName: "QuizMeAIIcon",
-                shortDescription: "app.description.quizmeai",
-                appId: "id6720763773",
-                releaseDate: Calendar.current.date(from: DateComponents(year: 2024, month: 10, day: 5))!,
-                category: .education
-            ),
-            GraceApp(
-                name: "app.name.localspeaks",
-                iconName: "LocalSpeaksIcon",
-                shortDescription: "app.description.localspeaks",
-                appId: "id6615060694",
-                releaseDate: Calendar.current.date(from: DateComponents(year: 2024, month: 8, day: 7))!,
-                category: .education
-            ),
-            GraceApp(
-                name: "app.name.itemizeai",
-                iconName: "ItemizeAIIcon",
-                shortDescription: "app.description.itemizeai",
-                appId: "id6737280335",
-                releaseDate: Calendar.current.date(from: DateComponents(year: 2024, month: 10, day: 27))!,
-                category: .utilities,
-                isExcluded: true
-            ),
-            GraceApp(
-                name: "app.name.chartybee",
-                iconName: "ChartYBeeIcon",
-                shortDescription: "app.description.chartybee",
-                appId: "id6740661428",
-                releaseDate: Calendar.current.date(from: DateComponents(year: 2025, month: 2, day: 10))!,
-                category: .utilities
-            ),
-            GraceApp(
-                name: "app.name.snapprogress",
-                iconName: "SnapProgressIcon",
-                shortDescription: "app.description.snapprogress",
-                appId: "id6745906297",
-                releaseDate: Calendar.current.date(from: DateComponents(year: 2025, month: 5, day: 20))!,
-                category: .utilities
-            ),
-            GraceApp(
-                name: "app.name.dialinespresso",
-                iconName: "DialInEspressoIcon",
-                shortDescription: "app.description.dialinespresso",
-                appId: "id6752831404",
-                releaseDate: Calendar.current.date(from: DateComponents(year: 2025, month: 9, day: 22))!,
-                category: .lifestyle
-            ),
-            GraceApp(
-                name: "app.name.fastinglady",
-                iconName: "FastingLadyIcon",
-                shortDescription: "app.description.fastinglady",
-                appId: "id6755406114",
-                releaseDate: Calendar.current.date(from: DateComponents(year: 2025, month: 11, day: 21))!,
-                category: .health
-            ),
-            GraceApp(
-                name: "app.name.herweigh",
-                iconName: "HerWeighIcon",
-                shortDescription: "app.description.herweigh",
-                appId: "id6757766090",
-                releaseDate: Calendar.current.date(from: DateComponents(year: 2026, month: 1, day: 15))!,
-                category: .health
-            ),
-        ]
+    private static let allGraceApps: [GraceApp] = [
+        GraceApp(
+            name: "app.name.tallycoin",
+            iconName: "TallyCoinIcon",
+            shortDescription: "app.description.tallycoin",
+            appId: "id1633932632",
+            releaseDate: Calendar.current.date(from: DateComponents(year: 2022, month: 7, day: 11))!,
+            category: .productivity
+        ),
+        GraceApp(
+            name: "app.name.readingclock",
+            iconName: "ReadingClockIcon",
+            shortDescription: "app.description.readingclock",
+            appId: "id6473550400",
+            releaseDate: Calendar.current.date(from: DateComponents(year: 2023, month: 11, day: 30))!,
+            category: .productivity
+        ),
+        GraceApp(
+            name: "app.name.stemcards",
+            iconName: "StemCardsIcon",
+            shortDescription: "app.description.stemcards",
+            appId: "id6478243260",
+            releaseDate: Calendar.current.date(from: DateComponents(year: 2024, month: 2, day: 27))!,
+            category: .education
+        ),
+        GraceApp(
+            name: "app.name.stitchtally",
+            iconName: "StitchTallyIcon",
+            shortDescription: "app.description.stitchtally",
+            appId: "id6738016114",
+            releaseDate: Calendar.current.date(from: DateComponents(year: 2024, month: 11, day: 12))!,
+            category: .lifestyle
+        ),
+        GraceApp(
+            name: "app.name.quizmeai",
+            iconName: "QuizMeAIIcon",
+            shortDescription: "app.description.quizmeai",
+            appId: "id6720763773",
+            releaseDate: Calendar.current.date(from: DateComponents(year: 2024, month: 10, day: 5))!,
+            category: .education
+        ),
+        GraceApp(
+            name: "app.name.localspeaks",
+            iconName: "LocalSpeaksIcon",
+            shortDescription: "app.description.localspeaks",
+            appId: "id6615060694",
+            releaseDate: Calendar.current.date(from: DateComponents(year: 2024, month: 8, day: 7))!,
+            category: .education
+        ),
+        GraceApp(
+            name: "app.name.itemizeai",
+            iconName: "ItemizeAIIcon",
+            shortDescription: "app.description.itemizeai",
+            appId: "id6737280335",
+            releaseDate: Calendar.current.date(from: DateComponents(year: 2024, month: 10, day: 27))!,
+            category: .utilities,
+            isExcluded: true
+        ),
+        GraceApp(
+            name: "app.name.chartybee",
+            iconName: "ChartYBeeIcon",
+            shortDescription: "app.description.chartybee",
+            appId: "id6740661428",
+            releaseDate: Calendar.current.date(from: DateComponents(year: 2025, month: 2, day: 10))!,
+            category: .utilities
+        ),
+        GraceApp(
+            name: "app.name.snapprogress",
+            iconName: "SnapProgressIcon",
+            shortDescription: "app.description.snapprogress",
+            appId: "id6745906297",
+            releaseDate: Calendar.current.date(from: DateComponents(year: 2025, month: 5, day: 20))!,
+            category: .utilities
+        ),
+        GraceApp(
+            name: "app.name.dialinespresso",
+            iconName: "DialInEspressoIcon",
+            shortDescription: "app.description.dialinespresso",
+            appId: "id6752831404",
+            releaseDate: Calendar.current.date(from: DateComponents(year: 2025, month: 9, day: 22))!,
+            category: .lifestyle
+        ),
+        GraceApp(
+            name: "app.name.fastinglady",
+            iconName: "FastingLadyIcon",
+            shortDescription: "app.description.fastinglady",
+            appId: "id6755406114",
+            releaseDate: Calendar.current.date(from: DateComponents(year: 2025, month: 11, day: 21))!,
+            category: .health
+        ),
+        GraceApp(
+            name: "app.name.herweigh",
+            iconName: "HerWeighIcon",
+            shortDescription: "app.description.herweigh",
+            appId: "id6757766090",
+            releaseDate: Calendar.current.date(from: DateComponents(year: 2026, month: 1, day: 15))!,
+            category: .health
+        ),
+    ]
 
-        let apps = allApps.filter { !$0.isExcluded }
+    public static func getAllApps(excluding appIdToExclude: String? = nil) -> [GraceApp] {
+        let apps = allGraceApps.filter { !$0.isExcluded }
         
         if let appIdToExclude {
             return apps.filter { $0.appId != appIdToExclude }
