b8d2a5558277941947bebbabedc840e4a6eb4dd3
higracehuang <higracehuang@gmail.com>
2026-04-08 17:53:18 -0500

    Update EmojiTextField.swift

--- Sources/GraceAppsLibrary/Views/EmojiTextField.swift
+++ Sources/GraceAppsLibrary/Views/EmojiTextField.swift
@@ -31,6 +31,24 @@ public class UIEmojiTextField: UITextField {
         }
         return nil
     }
+
+    public func addDoneButtonOnKeyboard() {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+
+        self.inputAccessoryView = doneToolbar
+    }
+
+    @objc func doneButtonAction() {
+        self.resignFirstResponder()
+    }
 }
 
 /// A SwiftUI wrapper for `UIEmojiTextField` that enforces single emoji/character input.
@@ -56,6 +74,7 @@ public struct EmojiTextField: UIViewRepresentable {
         if let font = font {
             emojiTextField.font = font
         }
+        emojiTextField.addDoneButtonOnKeyboard()
         return emojiTextField
     }
     
